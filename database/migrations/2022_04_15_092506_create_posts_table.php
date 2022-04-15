8920587a4bb0ad3bca167b9496cc68e8c539d9fe
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-04-15 18:04:59 +0500

    add migrations and models, debugging code

---
+++ database/migrations/2022_04_15_092506_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    private const TABLE_NAME = 'posts';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE_NAME, static function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('likes')->nullable();
+            $table->boolean('is_published')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE_NAME);
+    }
+}
---
+++ database/migrations/2022_04_15_124145_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    private const TABLE_NAME = 'comments';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE_NAME, static function (Blueprint $table) {
+            $table->id();
+            $table->string('Author')->nullable();
+            $table->text('text');
+            $table->date('created_at');
+            $table->unsignedBigInteger('likes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE_NAME);
+    }
+}
