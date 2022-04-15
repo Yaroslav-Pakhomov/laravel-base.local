65a7ded46bc82b149512eaf5f4d881341e0a892a
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-04-15 18:20:14 +0500

    add migrations and models, debugging code 2

--- database/migrations/2022_04_15_122211_create_tags_table.php
+++ database/migrations/2022_04_15_122211_create_tags_table.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types=1);
+
+declare(strict_types = 1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -8,6 +9,7 @@ use Illuminate\Support\Facades\Schema;
 class CreateTagsTable extends Migration
 {
     private const TABLE_NAME = 'tags';
+
     /**
      * Run the migrations.
      *
