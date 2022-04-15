c95db7c14fe0ec45a38202f48030b51905a62695
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-04-15 11:19:09 +0500

    init

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/page', function () {
+    return 'Create page';
+});
+
+Route::get('/page_news', function () {
+    return 'Create page_news';
+});
+
+Route::get('/page_contact', function () {
+    return 'Create page_contact';
+});
+
+Route::get('/page_about', function () {
+    return 'Create page_about';
+});
+
+Route::get('/page_catalog', function () {
+    return 'Create page_catalog';
+});
+
+Route::get('/page_categories', function () {
+    return 'Create page_categories';
+});
+
+Route::get('/page_blog', function () {
+    return 'Create page_blog';
+});
+
+Route::get('/page_tags', function () {
+    return 'Create page_tags';
+});
