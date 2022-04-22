391248f425790711a9d08494ae417a4d909ce458
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-04-22 09:31:36 +0500

    Factories and DatabaseSeeder classes (generation DB for Post, Tag, Category), debugging code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,14 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
+use JetBrains\PhpStorm\NoReturn;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +17,20 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    #[NoReturn] public function run(): void
     {
+        Category::factory(20)->create();
+        $tags = Tag::factory(50)->create();
+        // Создаём и записываем в БД 100 постов
+        $posts = Post::factory(200)->create();
+        foreach ($posts as $post):
+            // Получаем 5 случайных тегов и берём у них ID в виде массива
+            $tagsIDs = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsIDs);
+        endforeach;
+
+        // Создание массива, который не идёт в базу.
+        // $posts = Post::factory(1)->make();
         // \App\Models\User::factory(10)->create();
     }
 }
